1fa5c8c7b71c40f3f76fd9d874c32042f9a2c6ba
shivamk001 <shivam.kesarwani001@gmail.com>
2025-06-17 18:23:24 +0530

    refactor

--- client/app/users/login/page.tsx
+++ client/app/users/login/page.tsx
@@ -11,23 +11,9 @@ export default function LoginPage() {
     const dispatch = useAppDispatch();
     let [email, setEmail]=useState('');
     let [password, setPassword]=useState('');
-    let doRequest=useRequest({
-        url: 'http://localhost:8080/auth/signin',
-        body: {email,password},
-        method: METHOD.POST
-    })
 
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
-
-        const formData = new FormData(event.currentTarget)
-        const email = formData.get('email') ? `${formData.get('email')}` : '';
-        const password = formData.get('password') ? `formData.get('password')` : '';
-        console.log('USERNAME:', email, password);
-        setEmail(email as string);
-        setPassword(password as string);
-
-        // let response=await doRequest();
 
         let response = await dispatch(loginUser({email, password}));
 
